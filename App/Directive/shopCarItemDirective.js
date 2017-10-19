b8cfa2756f6cefacf90ccb9dc7201af8abeb93d8
weitianhao <973743887@qq.com>
2017-10-19 08:44:31 +0800

    first

---
+++ App/Directive/shopCarItemDirective.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Nan on 2017/7/25.
+ */
+app.directive('shopcarItem', function () {
+    return {
+        restrict: 'EA',
+        transclude: true,
+        scope:{
+            item:"=item",
+            index:"@index"
+        },
+        link:function(scope){
+            scope={
+                item:"=item",
+                index:"@index"
+            }
+        },
+        templateUrl: 'App/View/template/shoppingcarItem.html',
+        controller: 'shopCarItemController'
+    }
+});
